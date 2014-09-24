174b2f11a62fa57adc380422330a9d6260a75a12
unknown (author) <meriko@hotmail.com>
2014-09-24 22:59:49 +0200

    Allow for 20 videos per page

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -6,6 +6,9 @@ ICON   = 'icon-default.png'
 USER_AGENT = 'Mozilla/5.0 (iPad; CPU OS 7_0 like Mac OS X) AppleWebKit/537.51.1 (KHTML, like Gecko) Version/7.0 Mobile/11A465 Safari/9537.53'
 BASE_URL = 'http://nowthisnews.com'
 
+VIDEOS_PER_PAGE = 20
+SITE_VIDEOS_PER_PAGE = 10
+
 ###################################################################################################
 def Start():
     ObjectContainer.title1 = TITLE
@@ -33,6 +36,7 @@ def MainMenu():
         )
     )
 
+    # Parse page to get the other categories
     pageElement = HTML.ElementFromURL(BASE_URL)
     
     for item in pageElement.xpath("//*[@id='navigation']//li"):
@@ -59,49 +63,73 @@ def MainMenu():
     return oc
 
 ####################################################################################################
-@route(PREFIX + '/Videos', page = int)
-def Videos(title, url, page = 1):
+@route(PREFIX + '/Videos', page = int, offset = int)
+def Videos(title, url, page = 1, offset = 0):
     oc = ObjectContainer(title2 = title)
     
-    pageElement = HTML.ElementFromURL(url + "?page=" + str(page))
-    
-    for item in pageElement.xpath("//*[@id='playlist']//*[contains(@class,'entry')]"):        
-        videoURL = item.xpath(".//*[@class='info']//a/@href")[0]
-        
-        if not videoURL.startswith('http'):
-            videoURL = BASE_URL + videoURL
-            
-        videoTitle = item.xpath(".//*[@class='info']//a/text()")[0]
-        videoThumb = item.xpath(".//img/@src")[0]
-        videoSummary = item.xpath(".//*[@class='age']/text()")[0] + '\r\n\r\n' + videoTitle
-        
-        oc.add(
-            VideoClipObject(
-                url = videoURL,
-                title = videoTitle,
-                thumb = videoThumb,
-                summary = videoSummary
-            )
-        )
-        
+    [video_oc, page, offset, more] = GetVideos(url, page, offset, VIDEOS_PER_PAGE)
+
+    for object in video_oc.objects:
+        oc.add(object)
+ 
     if len(oc) < 1:
         oc.header  = "Sorry"
         oc.message = "Couldn't find any content"
-    else:
-        nextPage = pageElement.xpath("//*[@id='content']//a/@href")
+               
+    elif more:
+        oc.add(
+            NextPageObject(
+                key =
+                    Callback(
+                        Videos,
+                        title = title,
+                        url = url,
+                        page = page,
+                        offset = offset
+                    ),
+                title = 'More ...'
+            )
+        )
         
-        if nextPage:
+    return oc   
+
+####################################################################################################
+def GetVideos(url, page, offset, requestedNoVideos):
+    oc = ObjectContainer()
+    noVideos = 0 
+    requiredPages = (requestedNoVideos / SITE_VIDEOS_PER_PAGE) + 1
+    
+    for i in range(requiredPages): 
+        pageElement = HTML.ElementFromURL(url + "?page=" + str(page))
+        more = pageElement.xpath("//*[@id='content']//a[contains(@href,'?page=')]")
+        
+        for item in pageElement.xpath("//*[@id='playlist']//*[contains(@class,'entry')]")[offset:]:        
+            videoURL = item.xpath(".//*[@class='info']//a/@href")[0]
+            
+            if not videoURL.startswith('http'):
+                videoURL = BASE_URL + videoURL
+                
+            videoTitle = item.xpath(".//*[@class='info']//a/text()")[0]
+            videoThumb = item.xpath(".//img/@src")[0]
+            videoSummary = item.xpath(".//*[@class='age']/text()")[0] + '\r\n\r\n' + videoTitle
+            
             oc.add(
-                NextPageObject(
-                    key =
-                        Callback(
-                            Videos,
-                            title = title,
-                            url = url,
-                            page = page + 1
-                        ),
-                    title = 'More ...'
+                VideoClipObject(
+                    url = videoURL,
+                    title = videoTitle,
+                    thumb = videoThumb,
+                    summary = videoSummary
                 )
             )
-      
-    return oc
+            
+            noVideos += 1
+            offset += 1
+            
+            if noVideos >= requestedNoVideos:
+                return [oc, page, offset, more]
+    
+        if more:
+            page = page + 1
+            offset = 0
+        else:
+            return [oc, page, offset, more]
